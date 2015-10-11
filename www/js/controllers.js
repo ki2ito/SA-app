ec78cf5c16fecebf4aad3a28d684c3b38de29e9e
Guillaume Dick <guillaumedick@gmail.com>
2015-10-11 19:17:42 +0200

    init some views && controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,16 +41,333 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('ExercicesCtrl', function($scope, localStorage) {
+    $scope.methods = [
+        {
+            "name" : "Lafey",
+            "exercisesList" : [
+                {
+                    "name" : 'B',
+                    "description" : "Dips",
+                    "img" : ""
+                },
+                {
+                    "name" : 'A6',
+                    "description" : "Pompes au sol",
+                    "img" : ""
+                },
+                {
+                    "name" : 'A2',
+                    "description" : "Pompes surélevées",
+                    "img" : ""
+                },
+                {
+                    "name" : 'C3',
+                    "description" : "Tractions",
+                    "img" : ""
+                },
+                {
+                    "name" : 'G',
+                    "description" : "Abdos",
+                    "img" : ""
+                }
+            ]
+        },
+        {
+            "name": "Cardio",
+            "exercisesList": [
+                {
+                    "name": 'programme 1',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 2',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 3',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 4',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 5',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 6',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 7',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 8',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 9',
+                    "description": "test",
+                    "img": ""
+                },
+                {
+                    "name": 'programme 10',
+                    "description": "test",
+                    "img": ""
+                }
+            ]
+        }
+    ];
+
+    localStorage.set('methodsList', $scope.methods);
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('ExerciceCtrl', function($scope, $stateParams, localStorage) {
+
+        console.log($stateParams);
+
+        var methods = localStorage.get('methodsList');
+
+        $scope.exercise = methods[$stateParams.method].exercisesList[$stateParams.exercise];
+})
+
+.controller('ScheduleCtrl', function($scope, localStorage) {
+    $scope.schedule = [
+        {
+            "jour": "Lundi",
+            "exercisesList": [
+                {
+                    "name": 'B',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 12,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A6',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A2',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                }
+            ]
+        },
+        {
+            "jour" : "Mardi",
+            "exercisesList" : [
+                {
+                    "name" : 'C3',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 12,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'G',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 100,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'Vélo',
+                    "program": 3,
+                    "type" : 'cardio',
+                    "duree" : 90,
+                    "distance" : 50,
+                    "calories" : 1000
+                }
+            ]
+        },
+        {
+            "jour": "Mercredi",
+            "exercisesList": [
+                {
+                    "name": 'B',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 12,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A6',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A2',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                }
+            ]
+        },
+        {
+            "jour" : "Jeudi",
+            "exercisesList" : [
+                {
+                    "name" : 'C3',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 12,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'G',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 100,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'Vélo',
+                    "program": 3,
+                    "type" : 'cardio',
+                    "duree" : 90,
+                    "distance" : 50,
+                    "calories" : 1000
+                }
+            ]
+        },
+        {
+            "jour": "Vendredi",
+            "exercisesList": [
+                {
+                    "name": 'B',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 12,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A6',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                },
+                {
+                    "name": 'A2',
+                    "type": 'muscu',
+                    "series": 6,
+                    "repetitions": 16,
+                    "tempspause": 25,
+                    "tempsrecuperation": 25
+                }
+            ]
+        },
+        {
+            "jour" : "Samedi",
+            "exercisesList" : [
+                {
+                    "name" : 'C3',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 12,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'G',
+                    "type" : 'muscu',
+                    "series" : 6,
+                    "repetitions" : 100,
+                    "tempspause" : 25,
+                    "tempsrecuperation" : 120
+                },
+                {
+                    "name" : 'Vélo',
+                    "program": 3,
+                    "type" : 'cardio',
+                    "duree" : 90,
+                    "distance" : 50,
+                    "calories" : 1000
+                }
+            ]
+        },
+        {
+            "jour" : "Dimanche",
+            "exercisesList" : [
+                {
+                    "name" : 'REPOS',
+                    "type" : 'repos',
+                    "description" : "Jour de récupération"
+                }
+            ]
+        }
+    ];
+
+    localStorage.set('schedule', $scope.schedule);
+})
+
+    .controller('TrainingCtrl', function($scope, localStorage) {
+
+        //get date
+
+        //get schedule for this date
+        var schedule = localStorage.get('schedule');
+
+        var trainingDay = schedule[0];
+
+        $scope.training = [];
+
+        angular.forEach(trainingDay.exercisesList, function(value, key){
+
+            //données de base
+            var exercise = {
+                "name" : value.name,
+                "series" : value.series,
+                "repetitions" : value.repetitions,
+                "tempspause" : value.tempspause,
+                "tempsrecuperation" : value.tempsrecuperation,
+                "trainingScore" : {}
+            };
+
+            //set each repetition fot the form
+            for (var i = 0; i < value.series; i++) {
+
+                exercise.trainingScore["repetition"+(i+1)] = {"id" : "repetition"+(i+1), "value" : value.repetitions};
+            }
+
+            $scope.training.push(exercise)
+        });
+
+
+        //enregistre une fois la séance finie
+        //localStorage.set('training', $scope.training);
+    })
+
+;
